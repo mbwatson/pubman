62056f7a1270a03c790dc6970aae10776306b267
mbwatson <mattwat@gmail.com>
2018-08-03 08:47:27 -0400

    add authors with staff flag to publication serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from pubman.models import Publication
+from pubman.models import Publication, Author
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('name', 'is_staff')
 
 class PublicationSerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True)
+
     class Meta:
         model = Publication
-        fields = ('doi', 'title', 'citation')
+        fields = ('doi', 'title', 'citation', 'authors')
         lookup_field = ('doi')
